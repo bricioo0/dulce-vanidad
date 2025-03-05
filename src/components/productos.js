26ce10ee501e73e8b08aa444a087898d39ad16d6
bricio <briciopincha@gmail.com>
2025-03-05 15:40:49 -0300

    hola

---
+++ src/components/productos.js
@@ -0,0 +1,16 @@
+// src/components/Product.js
+import React from 'react';
+
+const Product = ({ product }) => {
+    return (
+        <div className="product">
+            <img src={product.imageUrl} alt={product.title} />
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <button>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
